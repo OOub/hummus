c2e8a401a10887464705066b819855eb91333b91
Omar Oubari <omaroubari@gmail.com>
2018-01-16 17:35:40 +0100

    started testing with 2D ATIS data
    Former-commit-id: 92dffd9bd6d4600a13bc95286946042867fbf5b4

--- applications/ATISNetwork.cpp
+++ applications/ATISNetwork.cpp
@@ -27,55 +27,58 @@ int main(int argc, char** argv)
 	auto teacher = dataParser.read1D("../../data/pip/1rec_4pips/teacher4pips_1type_2000reps.txt");
 
 	std::cout << "teacher size before resizing: " << teacher[0].size() << std::endl;
-	teacher.resize(repeatsInTeacher);
+	for (auto idx=0; idx<teacher.size(); idx++)
+	{
+		teacher[idx].resize(repeatsInTeacher);
+	}
 	std::cout << "teacher size after resizing: " << teacher[0].size() << std::endl;
 	
-////  ----- NETWORK PARAMETERS -----
-//	baal::Display network;
-//
-////  ----- INITIALISING THE NETWORK -----
-//	float runtime = data[0].back()+100;
-//	float timestep = 0.1;
-//
-//	float decayCurrent = 10;
-//	float potentialDecay = 20;
-//	float refractoryPeriod = 3;
-//    float efficacyDecay = 1000;
-//    float efficacy = 1;
-//
-//    int inputNeurons = 787;
-//    int layer1Neurons = 787;
-//
-//    float weight = 19e-10/10;
-//
-//	network.addNeurons(inputNeurons, decayCurrent, potentialDecay, refractoryPeriod, efficacyDecay, efficacy);
-//	network.addNeurons(layer1Neurons, decayCurrent, potentialDecay, refractoryPeriod, efficacyDecay, efficacy);
-//
-//	network.allToallConnectivity(&network.getNeuronPopulations()[0], &network.getNeuronPopulations()[1], false, weight, true, 20);
-//
-//	// starting the loggers
-////	network.learningLogger("learningLog_1.5jitter.txt");
-////	network.getNeuronPopulations()[1][data[1][1]].potentialLogger("potentialLog_9.5jitter.txt");
-//
-	// injecting spikes in the input layer
-//	for (auto idx=0; idx<data[0].size(); idx++)
-//	{
-//		network.injectSpike(network.getNeuronPopulations()[0][data[1][idx]].prepareInitialSpike(data[0][idx]));
-//    }
-//
-//	// injecting the teacher signal for supervised threshold learning
-//  	network.injectTeacher(&teacher);
-//
-////  ----- DISPLAY SETTINGS -----
-//	network.useHardwareAcceleration(true);
-//	network.setTimeWindow(5000);
-//	network.setOutputMinY(layer1Neurons);
-//	network.trackNeuron(787);
-//
-////  ----- RUNNING THE NETWORK -----
-//    int errorCode = network.run(runtime, timestep);
-//
-////  ----- EXITING APPLICATION -----
-//    return errorCode;
+//  ----- NETWORK PARAMETERS -----
+	baal::Display network;
+
+//  ----- INITIALISING THE NETWORK -----
+	float runtime = data[0].back()+100;
+	float timestep = 0.1;
+
+	float decayCurrent = 10;
+	float potentialDecay = 20;
+	float refractoryPeriod = 3;
+    float efficacyDecay = 1000;
+    float efficacy = 1;
+
+    int inputNeurons = 787;
+    int layer1Neurons = 787;
+
+    float weight = 19e-10/10;
+
+	network.addNeurons(inputNeurons, decayCurrent, potentialDecay, refractoryPeriod, efficacyDecay, efficacy);
+	network.addNeurons(layer1Neurons, decayCurrent, potentialDecay, refractoryPeriod, efficacyDecay, efficacy);
+
+	network.allToallConnectivity(&network.getNeuronPopulations()[0], &network.getNeuronPopulations()[1], false, weight, true, 20);
+
+	// starting the loggers
+//	network.learningLogger("learningLog_1.5jitter.txt");
+//	network.getNeuronPopulations()[1][data[1][1]].potentialLogger("potentialLog_9.5jitter.txt");
+
+	 injecting spikes in the input layer
+	for (auto idx=0; idx<data[0].size(); idx++)
+	{
+		network.injectSpike(network.getNeuronPopulations()[0][data[1][idx]].prepareInitialSpike(data[0][idx]));
+    }
+
+	// injecting the teacher signal for supervised threshold learning
+  	network.injectTeacher(&teacher);
+
+//  ----- DISPLAY SETTINGS -----
+	network.useHardwareAcceleration(true);
+	network.setTimeWindow(5000);
+	network.setOutputMinY(layer1Neurons);
+	network.trackNeuron(787);
+
+//  ----- RUNNING THE NETWORK -----
+    int errorCode = network.run(runtime, timestep);
+
+//  ----- EXITING APPLICATION -----
+    return errorCode;
     return 0;
 }
