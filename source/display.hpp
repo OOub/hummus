b82d92ef6c235eed4d2ecf851577a908e8da92f0
Omar Oubari <omaroubari@gmail.com>
2018-03-02 10:40:40 +0100

    ...
    Former-commit-id: d9c6350b2cf470be43588a6d3fc4c3780e154179

--- source/display.hpp
+++ source/display.hpp
@@ -4,7 +4,7 @@
  *
  * Created by Omar Oubari.
  * Email: omar.oubari@inserm.fr
- * Last Version: 22/02/2018
+ * Last Version: 02/03/2018
  *
  * Information: Add-on to the Network class, used to display a GUI of the spiking neural network output
  */
