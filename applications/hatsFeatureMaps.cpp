20716fcd796938ade2252732db1c0aef5ad6c43a
Omar Oubari <omaroubari@gmail.com>
2018-11-13 18:09:41 +0100

    ...
    Former-commit-id: 8275c08dfb5b31e8257ad354aef824c62b7018b4

--- applications/hatsFeatureMaps.cpp
+++ applications/hatsFeatureMaps.cpp
@@ -28,7 +28,6 @@ int main(int argc, char** argv)
     int layer0 = 0;
     int layer1 = 1;
     int layer2 = 2;
-	int layer3 = 3;
 	
 	int gridWidth = 42;
 	int gridHeight = 35;
@@ -44,25 +43,16 @@ int main(int argc, char** argv)
 	adonis_c::Stdp stdp(layer0, layer1);
 	
 	//  ----- CREATING THE NETWORK -----
-//	network.add2dLayer(layer0, rfSize, gridWidth, gridHeight, &stdp, 1, -1, false, decayCurrent, decayPotential, refractoryPeriod, burstingActivity, eligibilityDecay);
-//	network.addLayer(layer1, &stdp, 30, 1, 1, decayCurrent, decayPotential, refractoryPeriod, burstingActivity, eligibilityDecay);
-//	network.addLayer(layer2, nullptr, 1, 1, 1, decayCurrent, decayPotential, 100, burstingActivity, eligibilityDecay);
-//
-//	network.allToAll(network.getLayers()[layer0], network.getLayers()[layer1], false, 1./20, false);
-//	network.allToAll(network.getLayers()[layer1], network.getLayers()[layer2], false, 1./15, false);
-	
 	network.add2dLayer(layer0, rfSize, gridWidth, gridHeight, &stdp, 3, -1, false, decayCurrent, decayPotential, refractoryPeriod, burstingActivity, eligibilityDecay);
 	network.add2dLayer(layer1, rfSize, gridWidth, gridHeight, &stdp, 1, 1, false, decayCurrent, decayPotential, refractoryPeriod, burstingActivity, eligibilityDecay);
-//	network.add2dLayer(layer2, rfSize, gridWidth/2, gridHeight/2, nullptr, 1, 1, false, decayCurrent, decayPotential, refractoryPeriod, burstingActivity, eligibilityDecay);
-	network.addLayer(layer2, nullptr, 1, 1, 1, decayCurrent, decayPotential, 1300, burstingActivity, eligibilityDecay);
+	network.addLayer(layer2, nullptr, 1, 1, 1, decayCurrent, decayPotential, refractoryPeriod, burstingActivity, eligibilityDecay);
 
-	network.convolution(network.getLayers()[layer0], network.getLayers()[layer1], false, 1./15, false, 0);
-//	network.pooling(network.getLayers()[layer1], network.getLayers()[layer2], false, 1./4, false, 0);
-	network.allToAll(network.getLayers()[layer1], network.getLayers()[layer2], false, 1./10, false, 0);
+	network.convolution(network.getLayers()[layer0], network.getLayers()[layer1], false, 1./28, false, 0);
+	network.allToAll(network.getLayers()[layer1], network.getLayers()[layer2], false, 1./15, false, 0);
 	
 	//  ----- READING TRAINING DATA FROM FILE -----
 	adonis_c::DataParser dataParser;
-    auto trainingData = dataParser.readTrainingData("../../data/hats/feature_maps/nCars_train_100samplePerc_50rep.txt");
+    auto trainingData = dataParser.readTrainingData("../../data/hats/feature_maps/nCars_train_100samplePerc_1rep.txt");
 	
     //  ----- INJECTING TRAINING SPIKES -----
 	network.injectSpikeFromData(&trainingData);
@@ -72,20 +62,21 @@ int main(int argc, char** argv)
 	
 	//  ----- INJECTING TEST SPIKES -----
 	network.injectSpikeFromData(&testingData);
-	
+
 	// ----- ADDING LABELS
 	auto labels = dataParser.readLabels("" , "../../data/hats/feature_maps/nCars_test_100samplePerc_1repLabel.txt");
-	
+
 	network.addLabels(&labels);
 	
     //  ----- DISPLAY SETTINGS -----
   	qtDisplay.useHardwareAcceleration(true);
-  	qtDisplay.setTimeWindow(5000);
+  	qtDisplay.setTimeWindow(12000);
   	qtDisplay.trackLayer(2);
   	qtDisplay.trackInputSublayer(0);
   	qtDisplay.trackNeuron(network.getNeurons().back().getNeuronID());
 	
     //  ----- RUNNING THE NETWORK -----
+//    float runtime = trainingData.back().timestamp+1000;
     float runtime = testingData.back().timestamp+1000;
 	float timestep = 0.5;
 	
