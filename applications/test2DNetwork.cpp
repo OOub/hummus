a82e5124f30fb0f8f35ffccece732fa9ae705d2f
omar-ubuntu <omaroubari@gmail.com>
2019-01-17 14:21:28 +0100

    cleaning
    Former-commit-id: 7f21553ae9d282a7b3960c41731be8429285e6b0

--- applications/test2DNetwork.cpp
+++ applications/test2DNetwork.cpp
@@ -21,28 +21,28 @@ int main(int argc, char** argv)
 	//  ----- READING TRAINING DATA FROM FILE -----
 	adonis_c::DataParser dataParser;
 	auto trainingData = dataParser.readData("../../data/2Dtest.txt");
-	
+
     //  ----- INITIALISING THE NETWORK -----
 	adonis_c::QtDisplay qtDisplay;
 	adonis_c::Network network(&qtDisplay);
-	
+
 	//  ----- CREATING THE NETWORK -----
 	network.add2dLayer(2, 8, 8, {}, 2, -1);
 	network.add2dLayer(2, 8, 8, {}, 2, 1, false, false, 10, 20, 3, true);
 	network.addLayer({}, 1, 1, 1, false);
-		
+
 	network.allToAll(network.getLayers()[0], network.getLayers()[1], 1./2, 0);
 	network.allToAll(network.getLayers()[1], network.getLayers()[2], 1., 0);
-	
+
 	//  ----- DISPLAY SETTINGS -----
   	qtDisplay.useHardwareAcceleration(true);
   	qtDisplay.setTimeWindow(100);
   	qtDisplay.trackInputSublayer(0);
   	qtDisplay.trackLayer(1);
 	qtDisplay.trackNeuron(128);
-	
+
     //  ----- RUNNING THE NETWORK -----
-    network.run(0.1, &trainingData);
+    network.run(&trainingData, {}, 0.1);
 
     //  ----- EXITING APPLICATION -----
     return 0;
