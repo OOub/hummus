57127826c598ae40e47f081a266187e57d1db116
Omar Oubari <omaroubari@gmail.com>
2019-12-02 07:00:38 +0100

    ....

--- applications/ulpec_test.cpp
+++ applications/ulpec_test.cpp
@@ -31,18 +31,18 @@ int main(int argc, char** argv) {
     bool seed = true;
     
     // 3 class NMNIST
-    std::string training_path        = "/Users/omaroubari/Datasets/es_N-MNIST/Train";
-    std::string test_path            = "/Users/omaroubari/Datasets/es_N-MNIST/Test";
-    std::string gmap_filename        = "nmnist_3_g_maps.bin";
-    std::string label_filename       = "nmnist_3_labels.txt";
-    std::vector<std::string> classes = {"5", "6", "9"};
-    int percentage_data              = 100;
-    int logistic_start               = 0;
-    std::string tensor_base_name     = "nmnist_3";
-    bool multiple_epochs             = false;
-    int width                        = 28;
-    int height                       = 28;
-    int repetitions                  = 0;
+//    std::string training_path        = "/Users/omaroubari/Datasets/es_N-MNIST/Train";
+//    std::string test_path            = "/Users/omaroubari/Datasets/es_N-MNIST/Test";
+//    std::string gmap_filename        = "nmnist_3_g_maps.bin";
+//    std::string label_filename       = "nmnist_3_labels.txt";
+//    std::vector<std::string> classes = {"5", "6", "9"};
+//    int percentage_data              = 100;
+//    int logistic_start               = 0;
+//    std::string tensor_base_name     = "nmnist_3";
+//    bool multiple_epochs             = false;
+//    int width                        = 28;
+//    int height                       = 28;
+//    int repetitions                  = 0;
     
     // 10 class NMNIST
 //    std::string training_path        = "/Users/omaroubari/Datasets/es_N-MNIST/Train";
@@ -59,18 +59,18 @@ int main(int argc, char** argv) {
 //    int repetitions                  = 0;
     
     // 10 class NMNIST - 2 epochs
-//    std::string training_path        = "/Users/omaroubari/Datasets/es_N-MNIST/Train";
-//    std::string test_path            = "/Users/omaroubari/Datasets/es_N-MNIST/Test";
-//    std::string gmap_filename        = "nmnist_10_2e_g_maps.bin";
-//    std::string label_filename       = "nmnist_10_2e_labels.txt";
-//    std::vector<std::string> classes = {};
-//    int percentage_data              = 100;
-//    int logistic_start               = 0;
-//    std::string tensor_base_name     = "nmnist_10_2e";
-//    bool multiple_epochs             = true;
-//    int width                        = 28;
-//    int height                       = 28;
-//    int repetitions                  = 0;
+    std::string training_path        = "/Users/omaroubari/Datasets/es_N-MNIST/Train";
+    std::string test_path            = "/Users/omaroubari/Datasets/es_N-MNIST/Test";
+    std::string gmap_filename        = "nmnist_10_2e_g_maps.bin";
+    std::string label_filename       = "nmnist_10_2e_labels.txt";
+    std::vector<std::string> classes = {};
+    int percentage_data              = 100;
+    int logistic_start               = 0;
+    std::string tensor_base_name     = "nmnist_10_2e";
+    bool multiple_epochs             = true;
+    int width                        = 28;
+    int height                       = 28;
+    int repetitions                  = 0;
     
     // 4 class POKER-DVS 28x28 cropped
 //    std::string training_path        = "/Users/omaroubari/Datasets/es_POKER-DVS/Train";
@@ -186,7 +186,7 @@ int main(int argc, char** argv) {
         network.all_to_all<hummus::Memristor>(pixel_grid, output, 1, hummus::Uniform(1e-9, 1e-7, 0, 0, false), 100, -1);
         
         // running network asynchronously with spatial cropping down to 28x28 input and taking only the first N-MNIST saccade
-        network.verbosity(1);
+        network.verbosity(0);
         
         if (multiple_epochs) {
             // disabling propagation to the regression layer
