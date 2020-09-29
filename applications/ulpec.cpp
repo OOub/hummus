20cb5822e9c46af345c436a47ebb7ece1d3ac79d
Omar Oubari <omaroubari@gmail.com>
2020-09-29 14:55:54 +0200

    fix regression

--- applications/ulpec.cpp
+++ applications/ulpec.cpp
@@ -77,7 +77,7 @@ int main(int argc, char** argv) {
         // creating classifier
         hummus::layer classifier;
         if (logistic_regression) {
-            classifier = network.make_logistic_regression<hummus::Regression>(training_dataset, test_dataset, 0.1, 0, 0, 70, 128, 10, logistic_start, hummus::optimiser::SGD, tensor_base_name, 0, {});
+            classifier = network.make_logistic_regression<hummus::Regression>(training_dataset, test_dataset, 0.1, 0, 0, true, 70, 128, 10, logistic_start, hummus::optimiser::SGD, tensor_base_name, 0, {});
         } else {
             classifier = network.make_decision<hummus::Decision_Making>(training_dataset, test_dataset, 1000, 60, 0, {});
         }
@@ -148,7 +148,7 @@ int main(int argc, char** argv) {
             // creating classifier
             hummus::layer classifier;
             if (logistic_regression) {
-                classifier = network.make_logistic_regression<hummus::Regression>(training_dataset, test_dataset, 0.1, 0, 0, 70, 128, 10, logistic_start, hummus::optimiser::SGD, tensor_base_name+std::to_string(i), 0, {});
+                classifier = network.make_logistic_regression<hummus::Regression>(training_dataset, test_dataset, 0.1, 0, 0, true, 70, 128, 10, logistic_start, hummus::optimiser::SGD, tensor_base_name+std::to_string(i), 0, {});
             } else {
                 classifier = network.make_decision<hummus::Decision_Making>(training_dataset, test_dataset, 1000, 60, 0, {});
             }
